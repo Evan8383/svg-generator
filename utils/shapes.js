d2640e209e9e47312a312753a5c9d37b9e5ad8d6
evan8383 <evanbennett83@gmai.com>
2023-11-18 21:39:10 -0500

    Fixed bug where shape color was undefined.

--- utils/shapes.js
+++ utils/shapes.js
@@ -9,16 +9,16 @@ class Shape {
   }
 }
 class Circle extends Shape {
-  constructor(shapeColor) {
-    super(shapeColor)
+  constructor(text, textColor, shapeColor) {
+    super(text, textColor, shapeColor)
   }
   setShape() {
     return `<circle cx="150" cy="100" r="80" fill="${this.shapeColor}"/>`
   }
 }
 class Square extends Shape {
-  constructor(shapeColor) {
-    super(shapeColor)
+  constructor(text, textColor, shapeColor) {
+    super(text, textColor, shapeColor)
   }
   setShape() {
     return `<rect x="75" y="25" rx="5" ry="5" width="150" height="150" fill="${this.shapeColor}"/>`
